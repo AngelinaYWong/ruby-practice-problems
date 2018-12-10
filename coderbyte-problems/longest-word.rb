f1a97ed6dc127b141f6a47e5ea3d0feb50d4ef19
github username <angelina.w25@gmail.com>
2018-12-10 13:48:37 -0500

    changed answer to longest word

--- coderbyte-problems/longest-word.rb
+++ coderbyte-problems/longest-word.rb
@@ -1,15 +1,16 @@
 # Using the Ruby language, have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
 
 def LongestWord(sen)
-  var arr = sen.match(/[a-z0-9]+/gi);
+  
+    arr = sen.split.map do |w|
+      /[a-zA-Z0-9\s]+/.match(w)
+    end
 
-  var sorted = arr.sort(function(a, b) {
-    return b.length - a.length;
-  });
+    longest = arr.max_by do |w|
+        w.to_s.length
+    end
 
-  return sen
-
-end
+  return longest
 
 
 puts LongestWord(STDIN.gets)
