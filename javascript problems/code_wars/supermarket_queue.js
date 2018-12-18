734416a4faadd52abd3a22b50fad6bf6dc24b274
github username <angelina.w25@gmail.com>
2018-12-17 22:11:19 -0500

    added examples to problem

--- javascript problems/code_wars/supermarket_queue.js
+++ javascript problems/code_wars/supermarket_queue.js
@@ -3,3 +3,18 @@
 // customers: an array (list in python) of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
 // n: a positive integer, the number of checkout tills.
 // The function should return an integer, the total time required.
+// There is only ONE queue, and
+// The order of the queue NEVER changes, and
+// Assume that the front person in the queue (i.e. the first element in the array/list) proceeds to a till as soon as it becomes free.
+// 
+// queueTime([5,3,4], 1)
+// // should return 12
+// // because when n=1, the total time is just the sum of the times
+//
+// queueTime([10,2,3,3], 2)
+// // should return 10
+// // because here n=2 and the 2nd, 3rd, and 4th people in the
+// // queue finish before the 1st person has finished.
+//
+// queueTime([2,3,10], 2)
+// // should return 12
