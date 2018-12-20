b28e096ed1f613662734517dfbc8dbe349ee2ba4
github username <angelina.w25@gmail.com>
2018-12-20 12:29:32 -0500

    edited instructions

--- javascript problems/code_wars/mathematical_expression.js
+++ javascript problems/code_wars/mathematical_expression.js
@@ -15,8 +15,5 @@
 //
 // Parentheses
 // You need to support multiple levels of nested parentheses, ex. (2 / (2 + 3.33) * 4) - -6
-//
-// Whitespace
-// There may or may not be whitespace between numbers and operators.
-//
+
 // An addition to this rule is that the minus sign (-) used for negating numbers and parentheses will never be separated by whitespace. I.e., all of the following are valid expressions.
