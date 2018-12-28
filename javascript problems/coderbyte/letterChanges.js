3dea9905c12f0cd1247dca0c170f828ed484e933
github username <angelina.w25@gmail.com>
2018-12-28 13:59:02 -0500

    added solution

--- javascript problems/coderbyte/letterChanges.js
+++ javascript problems/coderbyte/letterChanges.js
@@ -10,7 +10,24 @@
 // Output:"gvO Ujnft!"
 function LetterChanges(str) {
 
-  return str;
+  str = str.trim().toLowerCase();
+var len = str.length;
+var newStr = '';
+
+for (var i = 0; i < len; i++) {
+  if (/[a-ce-gi-mo-su-y]/.test(str[i])) {
+    newStr += String.fromCharCode(((str[i].charCodeAt(0) - 18) % 26) + 97)
+  }
+  else if (/[zdhnt]/.test(str[i])) {
+      newStr += String.fromCharCode(((str[i].charCodeAt(0) - 18) % 26) + 65);
+  }
+  else {
+   newStr += str[i];
+  }
+}
+return newStr;
+
+}
 
 }
 
