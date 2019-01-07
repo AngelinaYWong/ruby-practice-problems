446c5b713599f3f28c410da61e81f734bcf6a796
github username <angelina.w25@gmail.com>
2019-01-07 16:38:17 -0500

    edited letterChanges

--- javascript problems/coderbyte/letterChanges.js
+++ javascript problems/coderbyte/letterChanges.js
@@ -6,20 +6,18 @@ function LetterChanges(str) {
 var len = str.length;
 var newStr = '';
 
-for (var i = 0; i < len; i++) {
-  if (/[a-ce-gi-mo-su-y]/.test(str[i])) {
-    newStr += String.fromCharCode(((str[i].charCodeAt(0) - 18) % 26) + 97)
+  for (var i = 0; i < len; i++) {
+    if (/[a-ce-gi-mo-su-y]/.test(str[i])) {
+      newStr += String.fromCharCode(((str[i].charCodeAt(0) - 18) % 26) + 97)
+    }
+    else if (/[zdhnt]/.test(str[i])) {
+        newStr += String.fromCharCode(((str[i].charCodeAt(0) - 18) % 26) + 65);
+    }
+    else {
+     newStr += str[i];
+    }
   }
-  else if (/[zdhnt]/.test(str[i])) {
-      newStr += String.fromCharCode(((str[i].charCodeAt(0) - 18) % 26) + 65);
-  }
-  else {
-   newStr += str[i];
-  }
-}
-return newStr;
-
-}
+  return newStr;
 
 }
 
