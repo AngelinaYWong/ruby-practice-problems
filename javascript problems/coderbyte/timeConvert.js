c0415866f6e2525764b5cd3c93d4d432c10bafd4
github username <angelina.w25@gmail.com>
2019-01-28 11:50:27 -0500

    outline of problem

--- javascript problems/coderbyte/timeConvert.js
+++ javascript problems/coderbyte/timeConvert.js
@@ -1,11 +1,21 @@
-Have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a colon.
+// Have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a colon.
+//
+// Examples:
+// Input:126
+//
+// Output:"2:6"
+//
+//
+// Input:45
+//
+// Output:"0:45"
 
-Examples:
-Input:126
+function TimeConvert(num) {
 
-Output:"2:6"
+  // code goes here
+  return num;
 
+}
 
-Input:45
-
-Output:"0:45"
+// keep this function call here
+TimeConvert(readline());
