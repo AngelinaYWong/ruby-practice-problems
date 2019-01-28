c322d534caa16849866dcca57de394d9fcc4e8d8
github username <angelina.w25@gmail.com>
2019-01-28 11:51:03 -0500

    added solution

--- javascript problems/coderbyte/timeConvert.js
+++ javascript problems/coderbyte/timeConvert.js
@@ -12,10 +12,14 @@
 
 function TimeConvert(num) {
 
-  // code goes here
-  return num;
+  var hours = Math.floor(num / 60);
+
+
+  var minutes = num % 60;
+
+
+  return hours + ':' + minutes;
 
 }
 
-// keep this function call here
-TimeConvert(readline());
+TimeConvert(124);
